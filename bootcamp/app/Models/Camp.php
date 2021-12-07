c0b079b7a21a43cd9cafdfe8af8a2e0febf95558
wahyuhjr <wahyuhidjr11@gmail.com>
2021-12-07 14:27:19 +0700

    camp db seeder

--- bootcamp/app/Models/Camp.php
+++ bootcamp/app/Models/Camp.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\softDeletes;
 
-class camps extends Model
+class camp extends Model
 {
     use HasFactory,softDeletes;
     protected $fillable = ['title','price'];
--- bootcamp/database/seeders/CampTableSeeder.php
+++ bootcamp/database/seeders/CampTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use app\Model\camps;
+use App\Models\Camp;
 
 class CampTableSeeder extends Seeder
 {
@@ -19,19 +19,22 @@ class CampTableSeeder extends Seeder
                 'title' => 'Gila Belajar',
                 'slug' => 'gila-belajar',
                 'price' => 280,
-                'created_at'=> date('Y-m-d H:i:s', time())
-                'update_at'=> date('Y-m-d H:i:s', time())
+                'created_at'=> date('Y-m-d H:i:s', time()),
+                'updated_at'=> date('Y-m-d H:i:s', time()),
             ],
             [
                 'title' => 'Baru Mulai',
                 'slug' => 'baru-mulai',
                 'price' => 140,
-                'created_at'=> date('Y-m-d H:i:s', time())
-                'update_at'=> date('Y-m-d H:i:s', time())
+                'created_at'=> date('Y-m-d H:i:s', time()),
+                'updated_at'=> date('Y-m-d H:i:s', time()),
             ],
-        ]; 
-             foreach ($camps as $key => $camp) {
-     Camp::create($camp);
-}
+        ];
+            //  foreach ($camps as $key => $camp) {
+            //  Camp::create($camp);
+            // }
+
+            //2nd method
+            Camp::insert($camps);
     }
 }
