c894e9bce71c5a226dd87f1fe870495aa6f073ca
wahyuhjr <wahyuhidjr11@gmail.com>
2021-12-09 13:46:45 +0700

    slicing login page

--- bootcamp/resources/views/login.blade.php
+++ bootcamp/resources/views/login.blade.php
@@ -8,23 +8,23 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/styles/main.css" type="text/css">
+    <link rel="stylesheet" href="{{asset('assets/styles/main.css')}}" type="text/css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 
-    <title>Laracamp by BuildWith Angga</title>
+    <title>Laracamp</title>
 </head>
 
 <body>
 
     <section class="login-user">
         <div class="left">
-            <img src="/assets/images/ill_login_new.png" alt="">
+            <img src="{{asset('assets/images/ill_login_new.png')}}" alt="">
         </div>
         <div class="right">
-            <img src="/assets/images/logo.png" class="logo" alt="">
+            <img src="{{asset('assets/images/logo.png')}}" class="logo" alt="">
             <h1 class="header-third">
                 Start Today
             </h1>
@@ -33,10 +33,10 @@
             </p>
             <p>
                 <a class="btn btn-border btn-google-login" href="#">
-                    <img src="/assets/images/ic_google.svg" class="icon" alt=""> Sign In with Google
+                    <img src="{{asset('assets/images/ic_google.svg')}}" class="icon" alt=""> Sign In with Google
                 </a>
             </p>
-            <img src="/assets/images/people.png" class="people" alt="">
+            <img src="{{asset('assets/images/people.png')}}" class="people" alt="">
         </div>
     </section>
 
