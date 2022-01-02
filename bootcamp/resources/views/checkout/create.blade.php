edb1336b1f3cb6d60207c248869bd5aed41f0c05
wahyuhjr <wahyuhidjr11@gmail.com>
2022-01-02 14:19:36 +0700

    validation done

--- bootcamp/resources/views/checkout/create.blade.php
+++ bootcamp/resources/views/checkout/create.blade.php
@@ -36,7 +36,7 @@
                                 <label class="form-label">Full Name</label>
                                 <input name="name" type="text"
                                     class="form-control {{$errors->first('name') ? 'is-invalid' : ''}}"
-                                    value="{{ Auth::user()->name }}" />
+                                    value="{{ Auth::user()->name }}" required />
                                 @if ($errors->has('name'))
                                 <p class="text-danger">{{$errors->first('name')}}</p>
                                 @endif
@@ -45,7 +45,7 @@
                                 <label class="form-label">Email Address</label>
                                 <input name="email" type="email"
                                     class="form-control {{$errors->first('name') ? 'is-invalid' : ''}}"
-                                    value="{{ Auth::user()->email }}" />
+                                    value="{{ Auth::user()->email }}" required />
                                 @if ($errors->has('email'))
                                 <p class="text-danger">{{$errors->first('email')}}</p>
                                 @endif
@@ -53,7 +53,7 @@
                             <div class=" mb-4">
                                 <label class="form-label">Occupation</label>
                                 <input name="occupation" type="text" class="form-control"
-                                    value="{{old('occupation') ? : Auth::user()->occupation }}" />
+                                    value="{{old('occupation') ? : Auth::user()->occupation }}" required />
                                 @if ($errors->has('occupation'))
                                 <p class="text-danger">{{$errors->first('occupation')}}</p>
                                 @endif
@@ -61,7 +61,7 @@
                             <div class="mb-4">
                                 <label class="form-label">Card Number</label>
                                 <input name="card_number" type="number" class="form-control"
-                                    value="{{old('card_number') ? : Auth::user()->card_number }}" />
+                                    value="{{old('card_number') ? : Auth::user()->card_number }}" required />
                                 @if ($errors->has('card_number'))
                                 <p class="text-danger">{{$errors->first('card_number')}}</p>
                                 @endif
@@ -70,11 +70,19 @@
                                 <div class="row">
                                     <div class="col-lg-6 col-12">
                                         <label for="exampleInputEmail1" class="form-label">Expired</label>
-                                        <input name="expired" type="month" class="form-control">
+                                        <input name="expired" type="month" class="form-control"
+                                            value="{{old('expired') ? : Auth::user()->expired }}" required>
+                                        @if ($errors->has('expired'))
+                                        <p class="text-danger">{{$errors->first('expired')}}</p>
+                                        @endif
                                     </div>
                                     <div class="col-lg-6 col-12">
                                         <label for="exampleInputEmail1" class="form-label">CVC</label>
-                                        <input name="cvc" type="number" class="form-control" maxlength="3">
+                                        <input name="cvc" type="number" class="form-control" maxlength="3"
+                                            value="{{old('cvc') ? : Auth::user()->cvc }}" required>
+                                        @if ($errors->has('cvc'))
+                                        <p class="text-danger">{{$errors->first('cvc')}}</p>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
@@ -94,5 +102,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integri
     ty="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
 </script>
+
 
 @endsection
