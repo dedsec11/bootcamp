10954677048348d0a547bf0fee8cd1c5e2c359fe
wahyuhjr <wahyuhidjr11@gmail.com>
2022-02-04 04:16:00 +0700

    midtrabs callback function

--- routes/web.php
+++ routes/web.php
@@ -26,36 +26,40 @@ Route::get('/', function () {
 
 
 //socialte google
-Route::get('sign-in-google',[UserController::class, 'google'])->name('user.login.google');
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
+//midtrans routes
+
+Route::get('payment/success', [UserController::class, 'midtransCallback']);
+Route::post('payment/success', [UserController::class, 'midtransCallback']);
 
 
 Route::middleware(['auth'])->group(function () {
-//checkout routes
-Route::get('checkout/success',[CheckoutController::class,'success'])->name('checkout.success')->middleware('EnsureUserRole:user');
-Route::get('checkout/{camp:slug}',[CheckoutController::class,'create'])->name('checkout.create')->middleware('EnsureUserRole:user');
-Route::post('checkout/{camp}',[CheckoutController::class,'store'])->name('checkout.store')->middleware('EnsureUserRole:user');
+    //checkout routes
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success')->middleware('EnsureUserRole:user');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create')->middleware('EnsureUserRole:user');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store')->middleware('EnsureUserRole:user');
 
-//dashboard
-Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+    //dashboard
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
 
-//User dashboard
-Route::prefix('user/dashboard')->namespace('User')->name('user.')->middleware('EnsureUserRole:user')->group(function () {
-    Route::get('/', [UserDashboard::class,'index'])->name('dashboard');
-});
+    //User dashboard
+    Route::prefix('user/dashboard')->namespace('User')->name('user.')->middleware('EnsureUserRole:user')->group(function () {
+        Route::get('/', [UserDashboard::class, 'index'])->name('dashboard');
+    });
 
-//Admin dashboard
-Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware('EnsureUserRole:admin')->group(function () {
-    Route::get('/', [AdminDashboard::class,'index'])->name('dashboard');
+    //Admin dashboard
+    Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware('EnsureUserRole:admin')->group(function () {
+        Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
 
-    //admin checkout
-    Route::post('admin/{checkout}', [AdminCheckout::class,'update'])->name('checkout.update');
-});
+        //admin checkout
+        Route::post('admin/{checkout}', [AdminCheckout::class, 'update'])->name('checkout.update');
+    });
 });
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
